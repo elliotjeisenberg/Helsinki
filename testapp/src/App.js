6aca1287b6dbaa323ac3d104703229f6f6da82f8
Elliot Eisenberg <elliotjeisenberg@gmail.com>
2022-08-17 00:28:18 -0500

    more files

--- testapp/src/App.js
+++ testapp/src/App.js
@@ -16,29 +16,31 @@ const Button = (props) => {
 
 
 const App = () => {
-  const [counter, setCounter] = useState(0)
+    const [left, setLeft] = useState(0)
+    const [right, setRight] = useState(0)
+    const [allClicks, setAll] = useState([])
 
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
+    const handleLeftClick = () => {
+        setAll(allClicks.concat('L'))
+        setLeft(left + 1)
+    }
 
-  return (
-    <div>
-      <Display counter={counter} />
-      <Button
-        onClick={increaseByOne}
-        text='plus'
-      />
-      <Button
-        onClick={setToZero}
-        text='zero'
-      />
-      <Button
-        onClick={decreaseByOne}
-        text='minus'
-      />
-    </div>
-  )
+    const handleRightClick = () => {
+        setAll(allClicks.concat('R'))
+        setRight(right + 1)
+    }
+
+    debugger
+
+    return (
+        <div>
+            {left}
+            <button onClick={handleLeftClick}>left</button>
+            <button onClick={handleRightClick}>right</button>
+            {right}
+            <p>{allClicks.join(' ')}</p>
+        </div>
+    )
 }
 
 export default App
